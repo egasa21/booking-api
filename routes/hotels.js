fa802482b336edd763667b8454c1f9628e4ac9cd
egasa21 <esaputra168@gmail.com>
2022-12-04 01:11:22 +0700

    commit

---
+++ routes/hotels.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { countByTpe, countByCity, createHotel, deleteHotel, getHotel, getHotelRooms, getHotels, updateHotel } from '../controllers/hotel.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
+
+const router = express.Router();
+
+router.post('/', verifyAdmin, createHotel);
+router.put('/:id', verifyAdmin,updateHotel);
+router.delete('/:id',verifyAdmin, deleteHotel);
+router.get('/find/:id', getHotel);
+
+router.get('/', getHotels);
+router.get('/countByCity', countByCity);
+router.get('/countByType', countByTpe);
+router.get('/room/:id', getHotelRooms);
+
+export default router;
